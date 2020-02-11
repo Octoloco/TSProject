3c368d16fb9f33e9b2afc4be4dfeef8dfc0b6602
Luis Silva <luansimo@outlook.com>
2020-02-10 23:28:59 -0600

    BasicMovement(incomplete)

---
+++ TwinSticksProject/Assets/Luis/Scenes/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical")).normalized * speed * Time.fixedDeltaTime;
+    }
+}

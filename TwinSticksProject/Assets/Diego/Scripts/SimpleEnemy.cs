061ac3188c8b2632c1f963849a4ccf8b14c129eb
Octoloco <diego_clorio@hotmail.com>
2020-02-10 22:08:18 -0600

    Day 1

---
+++ TwinSticksProject/Assets/Diego/Scripts/SimpleEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemy : MonoBehaviour
+{
+    public GameObject player;
+    enum EnemyState { Chase };
+    EnemyState currentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");    
+        currentState =
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

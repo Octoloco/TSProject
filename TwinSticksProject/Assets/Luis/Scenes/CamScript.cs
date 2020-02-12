76cac2c71cda0282e50b250a3ddef4b5de6e0fbb
Luis Silva <luansimo@outlook.com>
2020-02-11 20:34:27 -0600

    PlayerMovement(no rotation)

---
+++ TwinSticksProject/Assets/Luis/Scenes/CamScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour
+{
+    public GameObject player;
+    public float dist2Player;
+    public float camAngle;
+    private Vector3 plCamVector = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(camAngle, 0.0f, 0.0f);
+        plCamVector = new Vector3(0.0f, Mathf.Sin(Mathf.Deg2Rad*camAngle), - Mathf.Cos(Mathf.Deg2Rad * camAngle)) * dist2Player;
+        transform.position = player.transform.position + plCamVector;
+    }
+}
